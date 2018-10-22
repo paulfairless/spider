8781410fbc4dbb8955e1791396991cde60a91685
Paul Fairless <paul.fairless@gmail.com>
2018-10-22 22:51:25 +0100

    add images pipeline and firestore

--- spider/pipelines.py
+++ spider/pipelines.py
@@ -4,8 +4,41 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-
+from google.cloud import firestore
+from google.oauth2 import service_account
+import json
 
 class SpiderPipeline(object):
     def process_item(self, item, spider):
+        return item
+
+class FirestorePipeline(object):
+
+    collection_name = 'products'
+
+    def __init__(self, creds):
+        self.creds = creds
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            creds=json.loads(crawler.settings.get('FIREBASE')),
+        )
+
+    def open_spider(self, spider):
+        # print (self.creds)
+        credentials = service_account.Credentials.from_service_account_info(info=self.creds)
+        print credentials
+        self.db = firestore.Client(project='modum-e2bbb', credentials=credentials)
+        # pymongo.MongoClient(self.mongo_uri)
+        # self.db = self.client[self.mongo_db]
+
+    # def close_spider(self, spider):
+        # self.client.close()
+
+    def process_item(self, item, spider):
+        product = dict(item)
+        docRef = self.db.collection(self.collection_name).document(product['store']+'_'+product['sku'])
+        docRef.set(product)
+        # self.db[self.collection_name].document(dict(item))
         return item
--- spider/settings.py
+++ spider/settings.py
@@ -64,9 +64,11 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'spider.pipelines.SpiderPipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'spider.pipelines.SpiderPipeline': 300,
+   'scrapy.pipelines.images.ImagesPipeline': 1,
+   'spider.pipelines.FirestorePipeline': 1000
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/autothrottle.html
@@ -88,3 +90,21 @@ AUTOTHROTTLE_TARGET_CONCURRENCY = 0.5
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+AWS_ACCESS_KEY_ID='AKIAJZRV3YOR3KC7NI5Q'
+AWS_SECRET_ACCESS_KEY='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'
+IMAGES_STORE_S3_ACL = 'public-read'
+IMAGES_STORE = 's3://modum-image/download/'
+IMAGES_RESULT_FIELD = 'images'
+
+FIREBASE="""{
+  "type": "service_account",
+  "project_id": "modum-e2bbb",
+  "private_key_id": "2745fac00f4c47dbd6c2eb6cb40394661fa89bb7",
+  "private_key": "-----BEGIN PRIVATE KEY-----\\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDMU0fgow2paGGl\\nx4+XYgvN34r528AP9KMOCVlJverkXEhlj7OlAxpf+QvKgvrCwbpkj1QL6srKTVqp\\nbyRWEgKmAaX+82FTf+zEzlVbW+9tsM2egVHgDhiV3Q7OFPC79LZ+6j+q8XLogqQv\\nIGeXCIDe46GoyQbCjg8kZhzkW3aGOsWIQgP8mdmi45mfhivdSO5HErHKYA6f3x/O\\nPrlfnLWE0u5EugBgf9i3lb0yGKcmpmCtUaV4Cw91YrDGBfU+fy+0lEzubnQfKhII\\nBZZlq9hOJwNL8NMtGll+zGOEijCUZB6Gc6XGjdLOFohk67TnjUeRN6sELQ8KOqS3\\nRUpcRjL/AgMBAAECggEAN4UpstCblbzXS8S+L9RkgLyNdvdPaQaAh//iUzdX7FVn\\nFrHFjsEZGZsCqTSODv21OD36CIL0N6RtTjz1Eq4yKgmooW5gs9++5ntqljiVBqlU\\nNg3NwaQS6iX3L+hgwbHtJO9h/YE+SA+3rVaQz2lwGyCffM2jGJqhYrKchzCTNEWn\\nEuqrkSNoM5FniLbqzgXZ+xo3WnfCibJKiA5ihsf245sHWO0eYYncd3sbtCbKz4V2\\nUlb1fF+Mc/V7Gr/xtzafoiSVaXp1ZVb0KrqRpKftSGRS/RhNHT5I+f/tr3YCTvHb\\n0nDtDxBlLetDI2yerqphouU8oDYBI4yzsH89EkrlAQKBgQD9jFKOThbKBQxwab3X\\nIXD3j08TnNWOzEUw58kGUuuiByL//6F+5pmb6tH4DAW7FY7svL0pfli6UfN/riYF\\nYN7aZjPY+vwEPTbTY7r9AJ5752KqTbQ86MKNl9VXHmDL+Zfyim8YulLJ2Dn9DdMQ\\nyEWLRRPY8A9eAFY8U3U/awCruQKBgQDOTRp+QJUY8lh/FoYJrgicT4J9cwQ3lTMO\\n8+bxE+azpBI3lf6jxE3FLQeTtRqHAIbsWjF3yY1nfRwsB52D43yVWkbQNmGpniZm\\naLy8XgOsNcCA/ojNnpKW4Yv72OIQlIcDq8qYjGHYc+ZrY0pK8kxU1SBgmxfsA3um\\nTXWvJZlgdwKBgQDe4lq0RAseff+oVhwPIJOY2/7Mo+LLy9/LFQaPK5d98L4gwowp\\n7Pb9rBf18EOdaV8h1uyGrE29REtEY0eDFNvyq0NKq5+c7l7ixtnajI/FJeuDABnA\\nLXDRXbaqBcdbp8ad7HmrCCe8zfpz7JItP5B61DeVTbqEjNFB6K+6FwsY6QKBgHfy\\nLG0rb4TJotqBZf1KyobXU69MqC8I+F7BQIz6XTAlPNJEy4C8Kdtyiu0+ZuYHUGsr\\nJYfYD/cTrsWKx/Je2ZicnH+CntXXtcvlKZb7snb16a8WTsP+ZoyDJriPKSEqujVQ\\nNMLtHBaVjGrzl8ZriiXopKKGiioW/FV8w+fJnJuRAoGBAOXfvy2W+CgZasG7saA9\\nw88MkBWxwLvUuw8nFcXoiLLUidmXpPLv93TBdjc/xQmxjEU+6VEBNcIhA7cnaBmh\\nGRz430BxlDHSRl5N9x+YOPw5s850ZN5/8GI3dVPAVodxTHtPGNIRGtR1Xk7eAOeL\\nYXxmj298R1IC44pe2R8R+OyV\\n-----END PRIVATE KEY-----\\n",
+  "client_email": "firebase-adminsdk-p83pz@modum-e2bbb.iam.gserviceaccount.com",
+  "client_id": "106740250856022359029",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-p83pz%40modum-e2bbb.iam.gserviceaccount.com"
+}"""
